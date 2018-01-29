d5c2d5d6b7f55f394688c721c4991769f71b71ff
Veeck <github@veeck.de>
2018-01-29 18:04:18 +0100

    Fix copy task

--- bin/config.js
+++ bin/config.js
@@ -49,7 +49,7 @@ const config = {
 
     magnoliaLight: {
         src: [
-            `${basePaths.distSrcUI}`
+            `${basePaths.distSrcUI}/**/*`
         ],
         dest: basePaths.distTargetMagnolia
     },
